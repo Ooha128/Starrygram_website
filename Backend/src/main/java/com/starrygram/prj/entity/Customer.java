31719d66cc8ad4d7360215539a541f05071b795c
Ooha_128 <goohashree@gmail.com>
2023-10-12 11:17:35 +0530

    Added Frontend

--- Backend/src/main/java/com/starrygram/prj/entity/Customer.java
+++ Backend/src/main/java/com/starrygram/prj/entity/Customer.java
@@ -1,5 +1,14 @@
 package com.starrygram.prj.entity;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
 public class Customer {
 
 }
