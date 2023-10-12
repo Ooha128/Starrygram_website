3ce84db31ae2713a67b21a819c6385ebdda65e18
Ooha_128 <goohashree@gmail.com>
2023-10-12 11:10:39 +0530

    Adding Frontend

---
+++ Backend/src/main/java/com/starrygram/prj/StarrygramApplication.java
@@ -0,0 +1,13 @@
+package com.starrygram.prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarrygramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarrygramApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/starrygram/prj/StarrygramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starrygram.prj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarrygramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
