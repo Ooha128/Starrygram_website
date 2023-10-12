d552dc4161149ef5c636c25ad3f365f7e8f78ca4
Ooha_128 <goohashree@gmail.com>
2023-10-12 12:48:37 +0530

    Added Customer Entity

--- Backend/src/main/java/com/starrygram/prj/entity/Customer.java
+++ Backend/src/main/java/com/starrygram/prj/entity/Customer.java
@@ -1,5 +1,41 @@
 package com.starrygram.prj.entity;
 
-public class Customer {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "customers")
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	@Column(length = 50)
+	private String firstName;
+	
+	@Column(length = 50)
+	private String lastName;
+	
+	private String password;
+	
+	@Column(length = 10)
+	private String phoneNumber;
+	
+	
 }
